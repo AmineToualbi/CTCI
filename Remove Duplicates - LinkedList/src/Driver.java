20c48964f82afe17ea06266c33d536ba6e075caa
Amine <amine.toualbi@yahoo.fr>
2018-10-25 23:33:56 -0600

    Return Kth to Last - LinkedList

--- Remove Duplicates - LinkedList/src/Driver.java
+++ Remove Duplicates - LinkedList/src/Driver.java
@@ -49,25 +49,25 @@ public class Driver  {
 
 	public static void main(String[] args) {
 		
-//		LinkedListNode head = new LinkedListNode(10);
-//		head.next = new LinkedListNode(10);
-//		head.next.next = new LinkedListNode(10);
-//		head.next.next.next = new LinkedListNode(10);
-//		head.next.next.next.next = new LinkedListNode(10);
-//		head.next.next.next.next.next = new LinkedListNode(1);
-//		head.next.next.next.next.next.next = new LinkedListNode(11);
-//		head.next.next.next.next.next.next.next = new LinkedListNode(11);
+		LinkedListNode head = new LinkedListNode(10);
+		head.next = new LinkedListNode(10);
+		head.next.next = new LinkedListNode(10);
+		head.next.next.next = new LinkedListNode(10);
+		head.next.next.next.next = new LinkedListNode(10);
+		head.next.next.next.next.next = new LinkedListNode(1);
+		head.next.next.next.next.next.next = new LinkedListNode(11);
+		head.next.next.next.next.next.next.next = new LinkedListNode(11);
 		
-		LinkedListNode first = new LinkedListNode(0, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
-		LinkedListNode head = first;
-		LinkedListNode second = first;
-		for (int i = 1; i < 8; i++) {
-			second = new LinkedListNode(i % 2, null, null);
-			first.setNext(second);
-			second.setPrevious(first);
-			first = second;
-		}
-		
+//		LinkedListNode first = new LinkedListNode(0, null, null); //AssortedMethods.randomLinkedList(1000, 0, 2);
+//		LinkedListNode head = first;
+//		LinkedListNode second = first;
+//		for (int i = 1; i < 8; i++) {
+//			second = new LinkedListNode(i % 2, null, null);
+//			first.setNext(second);
+//			second.setPrevious(first);
+//			first = second;
+//		}
+//		
 		printList(head);
 		System.out.println();
 		removeDups(head);
